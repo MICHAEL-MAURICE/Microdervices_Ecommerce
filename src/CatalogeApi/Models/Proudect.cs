efdcf4d2c0c8e92d184b48bd944ce005c25df53b
Michael Maurice <M.Maurice@klickit.co>
2024-05-25 02:50:28 +0300

    Create Proudect

---
+++ src/CatalogeApi/Models/Proudect.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogeApi.Models
+{
+    public class Proudect
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Gategory { get; set; } = new();
+        public string Discription { get; set; }=default!;
+
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/CatalogeApi/Proudect/CreateProudect/CreateProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CatalogeApi.Models;
+using MediatR;
+
+namespace CatalogeApi.Proudect.CreateProudect;
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price):IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+//public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+//{
+//    public CreateProductCommandValidator()
+//    {
+//        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+//        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+//        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+//        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+//    }
+//}
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
