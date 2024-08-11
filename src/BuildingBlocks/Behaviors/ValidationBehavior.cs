528b989db90105eda77e2368c8324d4b9c1029ef
Michael Maurice <M.Maurice@klickit.co>
2024-08-11 23:47:56 +0300

    sync

--- src/BuildingBlocks/Behaviors/ValidationBehavior.cs
+++ src/BuildingBlocks/Behaviors/ValidationBehavior.cs
@@ -12,6 +12,8 @@ public class ValidationBehavior<TRequest, TResponse>
     {
         var context = new ValidationContext<TRequest>(request);
 
+
+
         var validationResults =
             await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
 
